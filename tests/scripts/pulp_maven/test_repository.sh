640f7dfc90a3c60325d39ce7869ffd5c730dfb7f
Matthias Dellweg <mdellweg@redhat.com>
2024-05-23 12:24:53 +0200

    Actually declare pulp-cli 0.25 compatibility

---
+++ tests/scripts/pulp_maven/test_repository.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# shellcheck source=tests/scripts/config.source
+. "$(dirname "$(dirname "$(realpath "$0")")")"/config.source
+
+pulp debug has-plugin --name "maven" || exit 23
+
+cleanup() {
+  pulp maven repository destroy --name "cli_test_maven_repo" || true
+}
+trap cleanup EXIT
+
+expect_succ pulp maven repository list
+
+expect_succ pulp maven repository create --name "cli_test_maven_repo" --description "Test repository for CLI tests"
+
+expect_succ pulp maven repository update --repository "cli_test_maven_repo" --description ""
+
+expect_succ pulp maven repository show --repository "cli_test_maven_repo"
+
+expect_succ pulp maven repository destroy --name "cli_test_maven_repo"
