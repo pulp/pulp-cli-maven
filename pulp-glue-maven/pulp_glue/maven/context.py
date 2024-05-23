f17d2c4d1632bbe26860ab2a0a152bde3fe3daed
Matthias Dellweg <mdellweg@redhat.com>
2024-05-23 12:24:53 +0200

    Actually declare pulp-cli 0.25 compatibility

--- pulp-glue-maven/pulp_glue/maven/context.py
+++ pulp-glue-maven/pulp_glue/maven/context.py
@@ -19,7 +19,7 @@ class PulpMavenArtifactContentContext(PulpContentContext):
     ENTITIES = _("artifact content")
     HREF = "maven_maven_artifact_href"
     ID_PREFIX = "content_maven_artifact"
-    NEEDS_PLUGINS = [PluginRequirement("maven", min="0.4.0")]
+    NEEDS_PLUGINS = [PluginRequirement("maven", specifier=">=0.4.0")]
 
 
 class PulpMavenDistributionContext(PulpDistributionContext):
@@ -29,10 +29,10 @@ class PulpMavenDistributionContext(PulpDistributionContext):
     ENTITIES = _("maven distributions")
     HREF = "maven_maven_distribution_href"
     ID_PREFIX = "distributions_maven_maven"
-    NEEDS_PLUGINS = [PluginRequirement("maven", min="0.4.0")]
+    NEEDS_PLUGINS = [PluginRequirement("maven", specifier=">=0.4.0")]
 
-    def preprocess_body(self, body: EntityDefinition) -> EntityDefinition:
-        body = super().preprocess_body(body)
+    def preprocess_entity(self, body: EntityDefinition, partial: bool = False) -> EntityDefinition:
+        body = super().preprocess_entity(body, partial=partial)
         version = body.pop("version", None)
         if version is not None:
             repository_href = body.pop("repository")
@@ -47,13 +47,13 @@ class PulpMavenRemoteContext(PulpRemoteContext):
     ENTITIES = _("maven remotes")
     HREF = "maven_maven_remote_href"
     ID_PREFIX = "remotes_maven_maven"
-    NEEDS_PLUGINS = [PluginRequirement("maven", min="0.4.0")]
+    NEEDS_PLUGINS = [PluginRequirement("maven", specifier=">=0.4.0")]
 
 
 class PulpMavenRepositoryVersionContext(PulpRepositoryVersionContext):
     HREF = "maven_maven_repository_version_href"
     ID_PREFIX = "repositories_maven_maven_versions"
-    NEEDS_PLUGINS = [PluginRequirement("maven", min="0.4.0")]
+    NEEDS_PLUGINS = [PluginRequirement("maven", specifier=">=0.4.0")]
 
 
 class PulpMavenRepositoryContext(PulpRepositoryContext):
@@ -63,9 +63,9 @@ class PulpMavenRepositoryContext(PulpRepositoryContext):
     ID_PREFIX = "repositories_maven_maven"
     VERSION_CONTEXT = PulpMavenRepositoryVersionContext
     CAPABILITIES = {
-        "add-cached-content": [PluginRequirement("maven", min="0.5.0.dev")],
+        "add-cached-content": [PluginRequirement("maven", specifier=">=0.5.0.dev")],
     }
-    NEEDS_PLUGINS = [PluginRequirement("maven", min="0.4.0")]
+    NEEDS_PLUGINS = [PluginRequirement("maven", specifier=">=0.4.0")]
 
     def add_cached_content(
         self, href: Optional[str] = None, body: Optional[EntityDefinition] = None
