381113e9f7e04a4c7f12537ace678c032c707688
pulpbot <pulp-infra@redhat.com>
2026-06-10 00:39:56 +0000

    Bump version: 0.5.1 → 0.5.2.dev

--- src/pulpcore/cli/maven/__init__.py
+++ src/pulpcore/cli/maven/__init__.py
@@ -12,7 +12,7 @@ from pulpcore.cli.maven.repository import repository
 translation = get_translation(__name__)
 _ = translation.gettext
 
-__version__ = "0.5.1"
+__version__ = "0.5.2.dev"
 
 
 @pulp_group(name="maven")
